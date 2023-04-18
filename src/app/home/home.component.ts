1e63f60d511de832b563752e5f6f764d17cf9da5
Rodrigo Neves <{ID}+{username}@users.noreply.github.com>
2023-04-18 19:51:40 -0300

    fix(card): fixed css card added flex and grid

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,16 +9,27 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit{
 
   titleCanadian = 'Dólar Canadense';
-  valueCanadian = 'R$ 4,37';
+  titleArgentino = 'Peso Argentino';
+  titleLibra = 'Libra Esterlina'
+  valueCanadian: any;
+  arraySize: any;
+  arrayValues: any;
+
   
-
-
   constructor(private http: HttpClient) { }
 
 
   ngOnInit() {
     this.http.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').subscribe((data) => {
       console.log(data);
+
+      // const valores = Object.values(data);
+      // console.log(valores);
+      // this.arrayValues = valores;
+
+      // const chaves = Object.keys(data);
+      // console.log(chaves)
+      // this.arraySize = chaves;
     });
   }
   
